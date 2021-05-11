e762ec4113cdfd7d25384e1e396a5da35a7193d3
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-11 09:30:38 +0300

    Create giris.py

---
+++ giris.py
@@ -0,0 +1,21 @@
+//degisken atama = ile yapilir.
+
+x = 5 
+
+//bir degiskenin veri tipi type() fonksiyonu ile ogrenilir / ciktisi alinir.
+
+type(x) = int
+
+/* aritmetik operatorler: toplama + , 
+  cikarma - , bolme / , carpma *, mod alma % */
+
+  5+5 = 10
+  5-5 = 0
+  5*5 = 25
+  5/5 = 1
+  5%5 = 0
+  
+// uslu sayilarla islem yaparken ** kullanilir. 
+    
+ 2**8 = 256
+
