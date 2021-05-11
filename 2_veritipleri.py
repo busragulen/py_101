938615ced65a2a7237b90174c91489e4d72e2b1c
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-11 09:50:43 +0300

    Create 2_veritipleri.py

---
+++ 2_veritipleri.py
@@ -0,0 +1,23 @@
+# string = sozel ifadeler (unicode dizi grubu)
+
+isim= 'X' /* type(isim)= str cikacaktir */ 
+print(isim) 
+
+# sonuc X cikar cunku veri tipi str 
+
+# bir degiskene string atarken = isaretinden sonra veri cift veya tek tirnak icerisinde yazilir.
+
+# int ve float sayisal verilerdir. float ondalik icin, int tam sayi icin kullanilir. veriler tirnak icine yazilmaz.
+
+a = 5
+type(a) /* ciktisi int olur */
+
+b = 6.1 
+type(b) /* ciktisi float olur */
+
+
+
+# len() fonksiyonu stringteki karakter sayisini cikti alır.
+
+len(isim) /* ciktisi 1 olacaktir */
+
