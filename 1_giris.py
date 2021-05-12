c3a458a7d5bc60e50338f9130b283a9d8e23c7ab
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-12 14:58:19 +0300

    Update 1_giris.py

--- 1_giris.py
+++ 1_giris.py
@@ -1,13 +1,12 @@
-//degisken atama = ile yapilir.
+# degisken atama = ile yapilir.
 
 x = 5 
 
-//bir degiskenin veri tipi type() fonksiyonu ile ogrenilir / ciktisi alinir.
+# bir degiskenin veri tipi type() fonksiyonu ile ogrenilir / ciktisi alinir.
 
 type(x) = int
 
-/* aritmetik operatorler: toplama + , 
-  cikarma - , bolme / , carpma *, mod alma % */
+# aritmetik operatorler: toplama + , cikarma - , bolme / , carpma *, mod alma
 
   5+5 = 10
   5-5 = 0
@@ -15,7 +14,7 @@ type(x) = int
   5/5 = 1
   5%5 = 0
   
-// uslu sayilarla islem yaparken ** kullanilir. 
+# uslu sayilarla islem yaparken ** kullanilir. 
     
  2**8 = 256
 
