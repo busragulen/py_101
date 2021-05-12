0ab7d2ff118ca870fc9cc1d7fd8e1f8de11b141b
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-12 14:59:19 +0300

    Update 1_giris.py

--- 1_giris.py
+++ 1_giris.py
@@ -1,3 +1,8 @@
+################## Degisken Atama ############################
+
+# Degisken isimleri sayi ile baslayamaz. 
+# Bosluk, aritmetik operatorler ve bazi isaretler kullanilamaz.
+# Uppercase ve lowercase karakterlere duyarlidir.
 # degisken atama = ile yapilir.
 
 x = 5 
@@ -5,6 +10,8 @@ x = 5
 # bir degiskenin veri tipi type() fonksiyonu ile ogrenilir / ciktisi alinir.
 
 type(x) = int
+
+############### Aritmetik Operatorler #######################
 
 # aritmetik operatorler: toplama + , cikarma - , bolme / , carpma *, mod alma
 
