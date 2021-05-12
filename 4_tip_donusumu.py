eb09e38d6792af7740dfd974903730b2381a9edf
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-12 17:55:36 +0300

    Create 4_tip_donusumu.py

---
+++ 4_tip_donusumu.py
@@ -0,0 +1,14 @@
+######### Tip Donusumu #########
+
+# int ve float veri tipleri arasinda donusum yapilmasi gerektiqinde asagidaki kodlarda gozuken metot, yani degiskene veri tipi atama yapilir.
+
+x=5
+p=float(x)
+
+# Bundan sonra type(p) yazarak aldigimiz outputta veri tipinin float oldugunu gorebiliriz. 
+# str() fonksiyonunun icindeki veriler string veri tipine donusturulur.
+####### NOT #######
+
+# Bir degisken dogrudan veri tipiyle birlikte atamak isteniyorsa, bu islem en basindan da yapilabilir:
+
+x=float(5)
