de346333c19f859fec6ed05bb13d114cd48f5d2a
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-15 09:01:29 +0300

    Create 10_mantiksal_baglac.py

---
+++ 10_mantiksal_baglac.py
@@ -0,0 +1,16 @@
+# Mantıksal bağlaçlar birden fazla koşulu kontrol etmemizi sağlar.
+
+##### VE bağlacı ########
+
+# python dilinde ve bağlacı and ile belirtilir. İki koşul da 1 ise sonuç 1 'dir, aksi takdirde sonuç 0'dır.
+
+##### VEYA bağlacı #######
+
+# python dilinde veya bağlacı or şeklinde belirtilir. İki koşuldan biri 1 ise sonuç 1'dir, aksi takdirde sonuç 0'dır.
+
+##### DEĞİL bağlacı ########
+
+# python dilinde değil bağlacı not şeklinde belirtilir.
+# bir koşulun değili, bir fonksiyonun tersi gibi düşünülebilir. 0'ın değili 1, 1'in değili 0'dır. True'nın False, False'ın True'dur.
+
+7>10 and 5>8 # false cevabını verecektir.
