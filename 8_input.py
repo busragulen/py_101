7f96c2336c2cb3021c15222283288dbcb77416b8
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-15 08:53:06 +0300

    Create 8_input.py

---
+++ 8_input.py
@@ -0,0 +1,9 @@
+# Kullanıcıdan girdi almak için input() fonksiyonu kullanılır.
+# Bir değişkene, kullanıcıdan alınan veriyi atamak için şu yöntem kullanılır:
+
+x=input("Lutfen bir deger giriniz:")  #Buna verilen cevap artık x'in değeri olur.
+
+# input() fonksiyonu ile bir değer girdiğimiz zaman değer varsayılan olarak string tipinde gönderilir. 
+# Bunu düzeltmek için input()'u bir int değişken dönüşümüne almak yeterlidir:
+
+y=int(input("Y degeri giriniz:"))
