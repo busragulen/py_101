3ff168265502c9a5ed2c5f08fd6069dd4c87c0c3
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-15 08:50:44 +0300

    Create sozlukler.py

---
+++ sozlukler.py
@@ -0,0 +1,17 @@
+# Sözlüklerin en büyük özelliği anahtar-değer ilişkisidir.
+# Sözlükler {} içerisinde tanımlanır. Sözlüklerin pythondaki adı dict şeklindedir.
+# Sözlüklerde indis kavramı yoktur. Anahtar kelime veya sayılardan yola çıkılır. Mesela "isim" bir anahtar iken Python onun değeridir.
+# Sözlüklere sonradan değer atanamaz.
+# Sözlüklere yeni bir anahtar eklemek için şu yöntem kullanılır:
+
+insan["sac"]="siyah"
+
+sozluk = dict() #ile de sözlük oluşturabiliriz.
+
+# Boş sözlük oluşturmak için ='dan sonra {} konması yeterlidir: 
+
+sayilar= {}
+
+# Sözlükteki bir değeri değiştirmek için anahtar ile işlem yapılır: 
+
+insan["isim"]= "Busra" #Yani yeniden atama yapıyoruz.
