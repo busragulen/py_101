08804cf16beec6896ce17a29bfbaa759861fea82
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-15 08:57:35 +0300

    Create 9_booleans.py

---
+++ 9_booleans.py
@@ -0,0 +1,4 @@
+# Mantıksal veri tipi boolean'dır.
+# Boolean'da True ve False olmak üzere iki değer vardır.
+# Boş listeyi bool()'a aldığımızda bize false, dolu listeyi aldığımızda ise true verir.
+# Bir durumu bir koşula sokarken, doğru mu yanlış mı olduğunu tespit etmede bool'dan yararlanırız.
