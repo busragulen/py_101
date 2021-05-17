1cbe8ad63a5993945b25e563d11de77542840ba4
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-17 13:05:44 +0300

    Create 19_metot.py

---
+++ 19_metot.py
@@ -0,0 +1,43 @@
+####### METOT NEDİR #######
+
+# Yazılımda özelleştirilmiş kod bloklarına verilen isimdir.
+# besbe tabanlı programların vazgeçilmezidir.
+# python'da metot nasıl kullanılır?
+
+# obje_ismi.metot(deger) şeklinde yazabiliriz.
+# örnek
+
+liste=[1,2,3]
+liste.append(4)
+print(liste) 
+
+# çıktısı 1,2,3 olr
+# liste objesinden devam edecek olursak, insert() diye bir fonksiyon da bulunmakta.
+# bu metotla bir indise, var olan degeri bir sonrakine kaydırmak yoluyala yeni bir değer ekleyebilirz. 
+
+liste=[1,2,3,4]
+liste.insert(1, 9)
+print(liste)
+
+# çıktısı  1,9,2,3,4 olacaktır.
+
+# listenin içini boşaltmak için .clear() metotu kullanılır
+
+isim.clear()
+print(isim)
+
+# çıktısı [] olur.
+
+# clear() metotunu vscode'da denerken fark ettim ki parantez içine indis verebiliyoruz
+# yani liste.clear(1) deseydik 1. indisi yani listedeki 9 degerini silecekti.
+
+# bir listede bir elemandan kaç tane olduğunu .count() ile görebiliriz.
+
+sayi=[1,1,1,2,2,2,3,3,3,5]
+sayi.count(1) # parantez içindeki değerden 3 tane olduğundan çıktısı 3 olur.
+
+# listedeki bir degeri silmek için .pop() kullanılır.
+
+liste=[1,2,3,4]
+liste.pop(0) # 0. indisi silecek
+print(liste) # çıktısında 0. indis yani 1 artık yok.
