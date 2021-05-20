12d8147077591aa3cd8b4d879f8b479c5d525b23
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-20 20:26:56 +0300

    Update 22_arbitrary_arguments.py

--- 22_arbitrary_arguments.py
+++ 22_arbitrary_arguments.py
@@ -1,1 +1,23 @@
-## 
+## tek parametre ile çoklu değer atama ##
+
+def toplam(x,y,z,w):
+  print("Toplam: ",x+y+z+w)
+toplama(4,5,6,7)
+
+# bu basit yöntemdi. ancak işlevselliği yok.
+# *args yöntemi:
+
+def toplama(*args):
+  print(args)
+toplama(4,5,6)
+
+#shift enterla yukarıdaki kodun çıktısını aldığımızda verdiğimiz değerlerden bir demet sunuyor.
+
+def toplama(*args):
+  toplam=0
+  for i in args:
+    toplam+=i
+    print("Toplam: ", toplam)
+    toplam=0
+    
+# bu algoritma tek parametre içinde birden fazla değerle işlem yapabiliriz. 
