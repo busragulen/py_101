83d6d37dcc3b15cf5acdd4d82c22f725c7803ff9
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-27 00:04:56 +0300

    Update 26_liste_parametresi.py

--- 26_liste_parametresi.py
+++ 26_liste_parametresi.py
@@ -1,1 +1,16 @@
+# bir fonksiyon içine listeyi nasıl atarız #
 
+# önce bir fonk oluşturalım
+
+def liste_oku(liste_adi):
+  for i in liste_adi:
+    print(i)
+isim=["x","y","z","t"]
+
+liste_oku(isim)
+
+#listemiz sayısal değerlerden oluşsaaydı.
+
+sayi=[1,2,3,4,5,6]
+liste_oku(sayi)
+#yine basarılı olacaktır
