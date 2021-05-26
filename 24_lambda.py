545729e7d8de439642e4936aaab41d3b8882c905
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-26 13:54:42 +0300

    Update 24_lambda.py

--- 24_lambda.py
+++ 24_lambda.py
@@ -1,1 +1,14 @@
+# Lambda #
+# lambda fonksiyonu, bir fonksiyonu tek satırda yazmamıza olanak sağlar
+# örnek olarak karekok adında bir fonk oluşturalım
 
+def karekok(x):
+  return x**0.5
+karekok(4) # çıktı 2 olur.
+
+#bunu lambda ile şöyle yazarız
+
+karekok=lambda x: x**0.5
+karekok(25) # boyle oldugunda çıktı olarak 5 verir.
+
+#yani aynı fonk'u tek satırda yazaraqk bu şekilde oluşturduk
