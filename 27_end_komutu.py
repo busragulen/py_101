62811355a6c702d4de3b6b38427c17b7725b3060
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-27 00:12:47 +0300

    Update 27_end_komutu.py

--- 27_end_komutu.py
+++ 27_end_komutu.py
@@ -1,1 +1,21 @@
+# end, py'da kullanışlı olan bir komuttur. #
 
+# mesela print() ile bir sey yazdırmak istersem:
+print("selam")
+print("github")
+#alt alta yazdırır
+#output şudur:
+# selam
+# github
+
+#direkt olarak yan yana yazdıracak olsaydık end kullanırdık...
+
+print("selam", end=" ") # eşittirin sağında default olaran \n var. biz istediğimiz karakteri koyabiliriz
+# ben boşluk koydum çünkü yan yana yazdırmak istiyorum
+print("github")
+
+#output şudur:
+# selam github
+
+## NOT:
+# boş bir şekilde print() fonk'u kullanmak bir satırı boş bıraktırır. 
