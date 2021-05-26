8e93b0e972d659ea504a2cc41dd53891bea8df06
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-27 00:39:47 +0300

    Update 28_enumerate_komutu.py

--- 28_enumerate_komutu.py
+++ 28_enumerate_komutu.py
@@ -1,1 +1,17 @@
+# bu fonksiyon sayesinde indexleme işlemleri daha kolay bir şekilde halledebilir
 
+liste=["x","y","z"]
+sayi=1
+for op in liste:
+  print(sayi,op)
+  sayi+=1
+  
+# böyle yaptığımda 1 x, 2 y, 3 z olarak alt alta output verecek.
+# bu kodu daha kolay ve daha kısa bir şekilde enumerate fonk'u ile yazarız:
+
+liste=["x","y","z"]
+for index,op in enumerate(liste,start=1): 
+  # burada op, listedeki bileşenleri döndürürken index de indexlerini döndürecek.
+  # enumerate()'e değer ekliyoruz: liste oluşturduk diye listeyi aldım. start dediğim sayıdan indexlemeye başlıyo
+  # start 1 olduğu için indis 0 da olsa ilk maddeye 1 verecek print fonksiyonu içinde
+    print(index,op)
