0b51a17b0a2047c5c1e204deca6e08d6a27e517a
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-30 01:50:25 +0300

    Create 33_time_modulu.py

---
+++ 33_time_modulu.py
@@ -0,0 +1,14 @@
+# tepkime süresi
+
+# koda tepkime süresi ekleme şeyi.
+# görüntü işlemede ihtiyacımız olacak
+
+import time
+print("selam")
+print("python")
+
+# bu iki komut arasında belli bir süre beklemek için:
+print("selam")
+time.sleep(5)
+print("python")
+# iki komut arasında 5 sn bekler
