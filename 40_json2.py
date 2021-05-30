f9414b00d9c2d05b3f19b6bc3abf7f8f7e56babf
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-30 22:43:47 +0300

    Update 40_json2.py

--- 40_json2.py
+++ 40_json2.py
@@ -1,1 +1,19 @@
+# py'da json kullanmak için json modülü ekleriz.
 
+import json
+kullanıcı='{"isim":"x", "soyisim":"y", "yas":1}' #şuan bu bir str
+#bu str'i json sayesinde bunu dicte dönüştürebiliriz
+
+info=json.loads(kullanıcı)
+info #shift enter yaptığımızda bir dict çıktısı alrız
+#type() ile de tekrardan kontrol edilebilir dict olduğu
+
+info["isim"] # output olarak x verecektri
+
+# örnek olarak isimdeki değeri değiştirmek istiyorsak:
+
+info["isim"] = "t" # artık shift+enter'da t verir.
+
+## sözlüğü direkt olarak jsona dönüştürmek
+kullanıcı=json.dumps(info)
+kullanıcı
