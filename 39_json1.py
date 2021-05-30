f7a5d78bb9681b8bb956c4ceb30fa956416c750d
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2021-05-30 21:34:21 +0300

    Create 39_json1.py

---
+++ 39_json1.py
@@ -0,0 +1,23 @@
+# json nedir
+
+# json, veri değişimini sağlayan bir programlama dilidir. birçok farklı dille birlikte kullanılabilmektedir
+# metinsel yazımı mevcuttur ve anahtar-değer ilişkisi ile programlanmaktadır.
+# çeşitli veri tipleri ile beraber de kullanılabilmektedir.
+
+# jsonda veri tipleri
+ 
+# karaktersel: string
+# sayısal: int, float
+# mantıksal:true ya da false
+# dizi: liste
+# nesne: anahtar-değer ilişkisine göre bir nesne tutabilir
+# örnek bir json kodu:
+{
+''isim'':''bsr''
+''soyisim'':''gln''
+''yas'':''17''
+''diller'':{''dil1'':''ing'',
+            ''dil2'':''alm''
+           }
+''mantiksal_bi_soru'':false
+} # ben hiç sevmedim bu syntaxı bu arada...
